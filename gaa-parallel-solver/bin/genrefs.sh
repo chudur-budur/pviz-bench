29452db86440a394752e6668de81289b0db3c2c1
chudur-budur <khaled.ahsan.talukder@gmail.com>
2020-10-11 12:48:36 -0400

    src to ./src

--- gaa-parallel-solver/bin/genrefs.sh
+++ gaa-parallel-solver/bin/genrefs.sh
@@ -6,9 +6,9 @@ if [ -d "data" ]; then
 fi
 
 echo "Generating 3112 reference directions suing Das-Dennis"
-matlab -nodisplay -nodesktop -nosplash -r "src/save_layer"
+matlab -nodisplay -nodesktop -nosplash -r "./src/save_layer"
 
 echo "Generating 3112 reference directions suing LHS"
-matlab -nodisplay -nodesktop -nosplash -r "src/save_lhs"
+matlab -nodisplay -nodesktop -nosplash -r "./src/save_lhs"
 
 echo "done"
