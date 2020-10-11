fd2fb2f6f5701cdb79f664d63f19688afaef4cf9
chudur-budur <khaled.ahsan.talukder@gmail.com>
2020-10-11 12:52:19 -0400

    fixing

--- gaa-parallel-solver/bin/genrefs.sh
+++ gaa-parallel-solver/bin/genrefs.sh
@@ -5,10 +5,12 @@ if [ -d "data" ]; then
     mkdir data
 fi
 
+cd src
+
 echo "Generating 3112 reference directions suing Das-Dennis"
-matlab -nodisplay -nodesktop -nosplash -r "./src/save_layer"
+matlab -nodisplay -nodesktop -nosplash -r "save_layer"
 
 echo "Generating 3112 reference directions suing LHS"
-matlab -nodisplay -nodesktop -nosplash -r "./src/save_lhs"
+matlab -nodisplay -nodesktop -nosplash -r "save_lhs"
 
 echo "done"
