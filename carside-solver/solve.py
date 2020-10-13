148f7e6a11ad57db435b579464d0ccb2482a0ffc
chudur-budur <khaled.ahsan.talukder@gmail.com>
2020-10-12 22:29:36 -0400

    cv added

--- carside-solver/solve.py
+++ carside-solver/solve.py
@@ -6,16 +6,17 @@ from pymoo.optimize import minimize
 from pymoo.visualization.scatter import Scatter
 
 # create the reference directions to be used for the optimization
-ref_dirs = get_reference_directions("das-dennis", 3, n_partitions=35)
-print(ref_dirs.size)
+ref_dirs = get_reference_directions("das-dennis", 3, n_partitions=40)
+print("ref_dirs.shape =", ref_dirs.shape)
 
 # create the algorithm object
-algorithm = NSGA3(pop_size=1998, ref_dirs=ref_dirs)
+algorithm = NSGA3(pop_size=ref_dirs.shape[0], ref_dirs=ref_dirs)
 
 # execute the optimization
-res = minimize(get_problem("carside"), algorithm, seed=1, termination=('n_gen', 600), verbose=True)
+res = minimize(get_problem("carside"), algorithm, seed=1, termination=('n_gen', 1000), verbose=True)
 
 # save the results
+np.savetxt("x.csv", res.X, delimiter=',', fmt='%1.4e')
 np.savetxt("f.csv", res.F, delimiter=',', fmt='%1.4e')
 np.savetxt("g.csv", res.G, delimiter=',', fmt='%1.4e')
-np.savetxt("x.csv", res.X, delimiter=',', fmt='%1.4e')
+np.savetxt("cv.csv", res.CV, delimiter=',', fmt='%1.4e')
